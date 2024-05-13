b9b4539d0e0779a281050cfcbf6a2890f961b6fc
Hadeer Salah <Haduirsalah@gmail.com>
2024-05-13 20:15:18 +0300

    Footer

--- src/app/download-company-profile/download-company-profile.component.ts
+++ src/app/download-company-profile/download-company-profile.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-download-company-profile',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './download-company-profile.component.html',
   styleUrl: './download-company-profile.component.scss'
 })
 export class DownloadCompanyProfileComponent {
-
+  
 }
